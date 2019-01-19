32b0271bc5100c8b076b77f2d11bf7dacd32b372
Student <student@localhost.ru>
2019-01-19 15:04:16 +0300

    Initial commit

---
+++ 01. JS Language/js/app.js
@@ -0,0 +1,14 @@
+import {calculateCashback} from "./cb.js";
+
+const amountEl = document.getElementById('amount');
+const amountEl2 = document.getElementById('amount2');
+const amountEl3 = document.getElementById('amount3');
+const calculateEl = document.getElementById('calculate');
+const resultEl = document.getElementById('result');
+
+function reactToClick() {
+    const amount = [parseInt(amountEl.value), parseInt(amountEl2.value), parseInt(amountEl3.value)];
+    resultEl.textContent = calculateCashback(amount);
+}
+
+calculateEl.addEventListener('click', reactToClick);

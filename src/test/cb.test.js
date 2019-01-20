66cdc0d20703e84ef4009005298a6c2d0d80194d
Student <student@localhost.ru>
2019-01-20 14:59:52 +0300

    Autotest fixed

--- src/test/cb.test.js
+++ src/test/cb.test.js
@@ -1,5 +1,9 @@
 import {calculateCashback} from "../js/cb.js";
-test('It should calculate cashback', () => {
+test('It should calculate under limit', () => {
     const result = calculateCashback([3000, 3000, 2300]);
     expect(result).toBe(810);
+});
+test('It should calculate over limit', () => {
+   const result = calculateCashback([7000,7000, 17000]);
+   expect(result).toBe(3000);
 });

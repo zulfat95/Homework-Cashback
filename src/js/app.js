6712198140d40247f282ce62e284683df7ccd128
Student <student@localhost.ru>
2019-01-24 20:24:19 +0300

    Fixed css, app, lib

--- src/js/app.js
+++ src/js/app.js
@@ -7,14 +7,15 @@ const calculateEl = document.getElementById('calculate');
 const resultEl = document.getElementById('result');
 
 function reactToClick() {
-    const amount = [parseInt(amountEl.value), parseInt(amountEl2.value), parseInt(amountEl3.value)];
-    resultEl.textContent = calculateCashback(amount) + 'руб';
-    // if (isNaN(amount)) {
-    //     resultEl.textContent = 'Вы не ввели сумму';
-    // }
-    // else {
-    //     return amount;
-    // }
+    const amount = amountEl.value;
+    const amount2 = amountEl2.value;
+    const amount3 = amountEl3.value;
+    if (isNaN(amountEl.value) || (isNaN(amountEl2.value)) || (isNaN(amountEl3.value))) {
+        resultEl.textContent = "Вы не ввели сумму в одно из полей";
+    }
+    else {
+        return resultEl.textContent = calculateCashback(amount, amount2, amount3) + " руб";
+    }
 }
 
 calculateEl.addEventListener('click', reactToClick);
--- src/js/cb.js
+++ src/js/cb.js
@@ -1,10 +1,11 @@
-export function calculateCashback(purchases) {
-
-    let result = (purchases[0] * 0.01) + (purchases[1] * 0.03) + (purchases[2] * 0.3);
+export function calculateCashback(min, big, extra) {
+    let minPurchase = min * 0.01;
+    let bigPurchase = big * 0.03;
+    let extraPurchase = extra * 0.3;
+    let result = minPurchase + bigPurchase + extraPurchase;
     const limit = 3000;
     if (result > limit) {
         return limit;
     }
-    return result;
-
+    return result.toFixed(2);
 }

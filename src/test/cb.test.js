096aae2346bf6b0021e13d6c5e9a4ebd71d2c4f8
Student <student@localhost.ru>
2019-01-24 20:31:55 +0300

    test fixed

--- src/test/cb.test.js
+++ src/test/cb.test.js
@@ -1,9 +1,9 @@
 import {calculateCashback} from "../js/cb.js";
 test('It should calculate under limit', () => {
     const result = calculateCashback(3000, 3000, 2300);
-    expect(result).toBe(810);
+    expect(result).toBe(810.00);
 });
 test('It should calculate over limit', () => {
-   const result = calculateCashback(7000,7000, 17000);
-   expect(result).toBe(3000);
+    const result = calculateCashback(7000,7000, 17000);
+    expect(result).toBe(3000);
 });

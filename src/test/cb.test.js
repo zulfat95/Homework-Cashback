e1ddae2a40f30af69addd538e01ac5ddfbf258e7
Student <student@localhost.ru>
2019-01-24 21:42:02 +0300

    Some fixes

--- src/test/cb.test.js
+++ src/test/cb.test.js
@@ -1,7 +1,7 @@
 import {calculateCashback} from "../js/cb.js";
 test('It should calculate under limit', () => {
     const result = calculateCashback(3000, 3000, 2300);
-    expect(+result).toBe(810);
+    expect(+result).toBe(810.00);
 });
 test('It should calculate over limit', () => {
     const result = calculateCashback(7000,7000, 17000);
